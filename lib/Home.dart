1c1a0db75ebb534fb5a6fb6a7be6d1e31df81357
Shadislam1 <shadislam999@gmail.com>
2025-02-04 19:16:00 +0600

    to do list cover

--- lib/Home.dart
+++ lib/Home.dart
@@ -11,28 +11,66 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  int _count = 0;
-  incrementCounter(){
+  List<String> _task = [];
+  TextEditingController _taskController =  TextEditingController();
 
-    setState(() {
-      _count++;
-    });
-    print(_count);
+  _addTask(){
+    if(_taskController.text.isNotEmpty){
+      setState(() {
+        _task.add(_taskController.text);
+        _taskController.clear();
+      });
+      print(_task);
+    }
   }
+  _removeTask(int index){
+    setState(() {
+      _task.removeAt(index);
+    });
+  }
+  _removeAll(){
+    setState(() {
+      _task.clear();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(" Pro"),
       ),
-      body: Center(
-        child: Text(_count.toString(),style: TextStyle(
-          fontSize: 50,
-        ),),
+      body:Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _taskController,
+              decoration: InputDecoration(
+                hintText: "Enter your task",
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(onPressed:_addTask, icon:Icon(Icons.add)),
+              ),
+            ),
+            SizedBox(height: 20,),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _task.length,
+                  itemBuilder: (context,index){
+                return Card(
+                  child: ListTile(
+                    title: Text(_task[index]),
+                    trailing: IconButton(
+                        onPressed: ()=> _removeTask(index),
+                        icon: Icon(Icons.delete,color: Colors.red,)),
+                  ),
+                );
+              }),
+            )
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: incrementCounter,child: Icon(Icons.add),
-      ),
+      floatingActionButton:FloatingActionButton(onPressed: _removeAll,child:Icon(Icons.delete_forever) ,) ,
     );
 
   }
